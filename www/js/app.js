b84cabfa154ac1897b5b591d67e67b0b2cc967c8
toby <smorgrav@yahoo-inc.com>
2015-08-13 09:15:40 +0200

    First take on parental mode with action sheet and camera integration

--- www/js/app.js
+++ www/js/app.js
@@ -51,10 +51,55 @@ angular.module('jboard', ['johans.services', 'ionic', 'ngCordova'])
 })
 
 
-.controller('ImageCtrl', function($scope, $cordovaMedia, $ionicLoading, MediaService, BoardService) {
+.controller('ImageCtrl', function($scope, $cordovaMedia, $ionicLoading, $ionicActionSheet, MediaService, BoardService, CameraService) {
+	
+	$scope.tileActionSheet = function() {
+	    $ionicActionSheet.show({
+	     buttons: [
+	       { text: 'New Picture' },
+		   { text: 'Voiceover' }
+	     ],
+	     destructiveText: 'Delete',
+	     titleText: 'Edit Tile',
+	     cancelText: 'Cancel',
+	     buttonClicked: function(index) {
+			 switch(index) {
+			     case 0:
+			         $scope.getPhoto();
+			         break;
+			     case 1:
+			         break;
+			     default:
+			         return;
+			 }
+	     }
+	   });
+	  };
+	  
+	  $scope.getPhoto = function() {
+	      console.log('Getting camera');
+	      Camera.getPicture({
+	        quality: 75,
+	        targetWidth: 320,
+	        targetHeight: 320,
+	        saveToPhotoAlbum: false
+	      }).then(function(imageURI) {
+	        console.log(imageURI);
+	        $scope.lastPhoto = imageURI;
+	      }, function(err) {
+	        console.err(err);
+	      })};
+		    
+	  
     $scope.onTap = function(r,c) {
         console.log("Tapped: " + r + ":" + c);
-        $scope.play("audio/test.wav")
+		$scope.tileActionSheet();
+		CameraService.getPicture().then(function(imageURI) {
+		      console.log(imageURI);
+		    }, function(err) {
+		      console.err(err);
+		    });
+      //  $scope.play("audio/test.wav")
     };
     
 	$scope.repeat = function(i) {
@@ -67,6 +112,10 @@ angular.module('jboard', ['johans.services', 'ionic', 'ngCordova'])
         });
     }
 	
+	$scope.getBoard = function(pos) {
+		BoardService.getBoards()[pos];		
+	}
+	
 	$scope.boards = BoardService.getBoards();
 	$scope.nofBoards =  BoardService.nofBoards();
     $scope.tiles = BoardService.getBoards()[0].tiles;
@@ -78,7 +127,6 @@ angular.module('jboard', ['johans.services', 'ionic', 'ngCordova'])
     }
 	
     $scope.parentMode = false;
-
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
--- www/js/services.js
+++ www/js/services.js
@@ -65,6 +65,24 @@ angular.module('johans.services', ['ionic'])
   return service;
 })
 
+.factory('CameraService', ['$q', function($q) {
+
+  return {
+    getPicture: function(options) {
+      var q = $q.defer();
+
+      navigator.camera.getPicture(function(result) {
+        // Do any magic you need
+        q.resolve(result);
+      }, function(err) {
+        q.reject(err);
+      }, options);
+
+      return q.promise;
+    }
+  }
+}])
+
 .factory('BoardService', function(){
     var service = {
       getBoards: getBoards,
